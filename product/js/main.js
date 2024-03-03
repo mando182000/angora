6206767baf56907b796b2e1c06dee44a9dfa28f8
mando182000 <156642216+mando182000@users.noreply.github.com>
2024-03-03 09:11:47 +0200

    first commit

---
+++ product/js/main.js
@@ -0,0 +1,9 @@
+var Bookmark yourfavoritesites =document.getElementById('site name')
+var Bookmark url=document.getElementById('site url')
+
+function addSitecontact ()
+
+{
+    console.log(sitename, siteurl);
+}
+addSitecontact()
